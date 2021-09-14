274904fd2b9bfddaa14aa6cec750f7fa6d8e088a
Filippo Ferroni <naffe15@yahoo.it>
2021-09-14 18:22:36 -0500

    Update example_12_bdfm.m
    minor adjustment: added a few commented lines and pc

--- examples/BVAR tutorial/example_12_bdfm.m
+++ examples/BVAR tutorial/example_12_bdfm.m
@@ -13,57 +13,32 @@ rng('default');
 rng(999);
 
 % generate factors/observables
-%T   = 301;
-% Phi = [0.75  0.1   0.1;
-%        0.0   0.5   0.0;
-%        0.0   0.0   0.5];
-% 
-% Sigma = [1     0.01   0.01;
-%          0.01   1     0.023;
-%          0.01   0.023   2];
-% Q   = chol(Sigma,'lower');
-% f   = zeros(T,size(Phi,1));
-% Lambda = [1    0      0;
-%           0.3  1      0; 
-%           0.5 -1.5    1;
-%          -1.1 -0.5  0.8;
-%           1.5  1.5  -.5;
-%            randn(45,size(Phi,1))];
-%        
-% Phi = [0.75  0.1;
-%     0.0   0.25];
-% 
-% Sigma = [1     0.25;
-%          0.25    3];
-% Q   = chol(Sigma,'lower');
-% f   = zeros(T,size(Phi,1));
-% Lambda = [1    0;
-%          0.3  1;
-%          0.5 -1.5;
-%         -1.1 -0.5;
-%          1.5  1.5;
-%          randn(45,size(Phi,1))];
 
-T      = 301;
+% Setting the parameters
+% AR1 factors
 Phi    = [0.75 0.0;0.1 0.8];
-f      = zeros(T,size(Phi,1));
+% Cov factors innovation
+Sigma  = [1 -0.2; -0.2 2];
+Q      =  chol(Sigma,'lower');
+% factor loadings
 Lambda = [1 0;
           0.3 1;
           0.5 -1.5;
           -1.1 -0.5;
           1.5 1.5;            
-      5*randn(45,size(Phi,1))];       
-     
-     
+      5*randn(45,size(Phi,1))];  
+% persistence of idisyncratic terrors
+rho    = 0; 
+% standard deviation of idisyncratic errors
+sig    = 1;
+
+% preallocating memory
+% sample length
+T      = 301;
+f = zeros(T,size(Phi,1));
 y = zeros(T,size(Lambda,1));
 e = zeros(T,size(Lambda,1));
 i = zeros(T,size(Phi,1));
-
-rho    = 0;
-sig    = 1;
-Sigma  = [1 -0.2; -0.2 2];
-Q      =  chol(Sigma,'lower');
-
 
 for t = 1 : T-1
     i(t+1,:) = randn(size(f,2),1)';
@@ -73,7 +48,6 @@ for t = 1 : T-1
 end
 f(1,:) = [];
 y(1,:) = [];
-e(1,:) = [];
 
 subplot(2,1,1)
 plot(y)
@@ -93,29 +67,52 @@ else
 end
 
 
+%% principal components
+
+nfac   = 2;
+transf = 0;
+
+[~,pc,~,egv,~] = pc_T(y,nfac,transf);
+
+% scree plot
+figure,
+plot(egv(1:10),'b');
 
 
 %% static factor model
+
+% 2 static factors
+nfac = size(f,2);
+lags = 0; 
+% priors
 options.priors.F.Lambda.mean = 0;
 options.priors.F.Lambda.cov  = 6;
-
-nfac   = size(f,2);
-[BSFM] = bdfm_(y,0,nfac,options);
-
+% estimation command
+[BSFM] = bdfm_(y,lags,nfac,options);
+% consider a subset of draws
 index = 5000:20:size(BSFM.Phi_draws,3);
+% plot estimated and true factors
 figure,
 for gg=1:nfac
     subplot(nfac,1,gg)
+    % draws of factors
     plot([squeeze(BSFM.f_draws(:,gg,index))],'Color',[0.7 0.7 0.7])
     hold on
+    % true factors
     plot(f(:,gg),'b','Linewidth',2)
     hold on
     plot(mean(BSFM.f_draws(:,gg,index),3),'k','Linewidth',2)
 end
+% save plot
+dirname  = 'factor_plt';
+mkdir(dirname)
+STR_RECAP = [ dirname '/sfm'];
+saveas(gcf,STR_RECAP,'fig');
+savefigure_pdf([STR_RECAP '.pdf']);
 
 
 %% dynamic factor model
-
+clear options
 nfac   = size(f,2);
 lags   = round(size(Phi,2)/nfac);
 
@@ -130,21 +127,17 @@ options.priors.F.Lambda.cov  = 6;
 % idyosincratic error priors
 options.priors.G.Sigma.scale = sig;
 options.priors.G.Sigma.df    = 4;
-
 % IRF options
 % IV identification
 options.proxy    = i(lags+2:end,1);
 % Sign identification
 options.signs{1} = 'y(4,1:3,1)<0'; %
 options.signs{2} = 'y(5,1:3,1)>0'; %
-
-
+% estimation command
 [BDFM] = bdfm_(y,lags,nfac,options);
-
-
-%%
-
+% consider a subset of draws
 index = 5000:20:size(BDFM.Phi_draws,3);
+% plot estimated and true factors
 figure,
 for gg=1:nfac
     subplot(nfac,1,gg)
@@ -154,7 +147,11 @@ for gg=1:nfac
     hold on
     plot(mean(BDFM.f_draws(:,gg,index),3),'k','Linewidth',2)
 end
+STR_RECAP = [ dirname '/dfm'];
+saveas(gcf,STR_RECAP,'fig');
+savefigure_pdf([STR_RECAP '.pdf']);
 
+%%
 
 Phi_m = mean(BDFM.Phi_draws(:,:,index),3);
 Sig_m = mean(BDFM.Sigma_draws(:,:,index),3);
@@ -233,13 +230,18 @@ options.add_irfs   = ir_true(index_var,:,index_sho);
 options.conf_sig_2 = 0.9;   
 % additional 90% HPD set
 options.nplots = [2 3];   
-
+% name of the directory where the figure is saved
+options.saveas_dir    = './factor_plt';
+% name of the figure to save
+options.saveas_strng  = 'sign';
 % sign restricted IRF
 irfs_to_plot       = BDFM.irsign_draws(index_var,:,index_sho,:);
 plot_irfs_(irfs_to_plot,options)
 
 % IV IRF
 irfs_to_plot_iv     = BDFM.irproxy_draws(index_var,:,index_sho,:);
+% name of the figure to save
+options.saveas_strng  = 'iv';
 plot_irfs_(irfs_to_plot_iv,options)
 
 
