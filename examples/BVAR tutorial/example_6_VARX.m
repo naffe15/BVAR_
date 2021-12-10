aef7768c86937ebca3637c0acb7084bd1d2aafd5
Filippo Ferroni <naffe15@yahoo.it>
2021-12-10 11:30:22 -0600

    Commented command lines

--- examples/BVAR tutorial/example_6_VARX.m
+++ examples/BVAR tutorial/example_6_VARX.m
@@ -16,12 +16,11 @@ load DataPooling
 % Time:         1978m1 to 2012m8
 cnames = {'uk','us','jp','de'};   % names of the countries
 Nc = length(cnames);
-% Varible names:    IPI, CPI, 1Y GOVT YIELD (LTR), Policy Rate (STR)
+% Varible names: IPI, CPI, 1Y GOVT YIELD (LTR), Policy Rate (STR)
 vnames = {'ipi','cpi','ltr','str'};
 Nv = length(vnames);
 
 T       = size(time,1)-1;
- 
  
 % pick US rate (2,4) as  exogenous in a  VAR with UK variables(1:1;4)
 lags        = 4 ;
@@ -33,19 +32,19 @@ y = demean(100*diff(log ([ipi_uk ,cpi_uk , ltr_uk , str_uk]) )) ;
 z = demean(100*diff(log (str_us)));
 % include one lag for the exogenous variable
 options.controls = lagX(z,[0:1]);
-% % signs res
+
+% % signs/narrative restrictions
 % options.signs{1}     = 'y(1,1:3,1)>0'; % 
 % options.signs{2}     = 'y(2,1:3,1)<0'; % 
 % options.narrative{1} = 'v([3],1)>0'; 
-
-% specify the restrictions
-% 1) ad = aggregate demand disturbance [sign restrictions]
-options.zeros_signs{1}     = 'y(1,1)=1;';
-options.zeros_signs{end+1} = 'y(2,1)=1;'; 
-options.zeros_signs{end+1} = 'y(3,1)=1;';
-% 2) as = aggregate supply shock [sign restrictions]
-options.zeros_signs{end+1} = 'y(1,2)=1;';
-options.zeros_signs{end+1} = 'y(2,2)=-1;';
+% % zero/sign restrictions
+% % 1) ad = aggregate demand disturbance [sign restrictions]
+% options.zeros_signs{1}     = 'y(1,1)=1;';
+% options.zeros_signs{end+1} = 'y(2,1)=1;'; 
+% options.zeros_signs{end+1} = 'y(3,1)=1;';
+% % 2) as = aggregate supply shock [sign restrictions]
+% options.zeros_signs{end+1} = 'y(1,2)=1;';
+% options.zeros_signs{end+1} = 'y(2,2)=-1;';
 
 % estimate the VARX
 bvar1       = bvar_(y,lags,options); 
@@ -55,15 +54,15 @@ indx_sho              = 1;
 indx_var              = [1, 2, 3, 4];
 irfs_to_plot           = bvar1.irx_draws(indx_var,:,indx_sho,:);
 
-
 % variables names for the plots
 options.varnames      = {'UK IP','UK CPI', 'UK Long rate', 'UK Short rate'};  
 % name of the shock
- options.shocksnames   = {'US Short rate shock'};
+options.shocksnames   = {'US Short rate shock'};
 
-options.conf_sig_2    = 0.95;    % additional 95% HPD set
-options.nplots = [1 4];          % plot appeareance
+options.conf_sig_2    = 0.95;           % additional 95% HPD set
+options.nplots        = [1 4];          % plot appeareance
 options.saveas_strng  = 'VARX';         % name  of  the figure  to  save
 options.saveas_dir    = './VARX_plt';   % folder where  it  is  stored
 % the plotting command
 plot_all_irfs_(irfs_to_plot,options);
+
