6be1a5b3821ea58f21f564168c498cea1bd47423
Filippo Ferroni <naffe15@yahoo.it>
2025-10-20 10:48:24 +0200

    Update nowcast_bvar.m
    add foreacast to the nowcast function

--- bvartools/nowcast_bvar.m
+++ bvartools/nowcast_bvar.m
@@ -4,6 +4,9 @@ fast_kf             = 0;
 KFoptions           = BVAR.KFoptions;
 NumbDrawsFromPost   = 100;
 lags                = BVAR.lags;
+fhor                = BVAR.fhor;
+forecast_data       = BVAR.forecast_data;
+
 [T, ny, nD]         = size(DataMFNC);
 
 if nargin>2
@@ -21,6 +24,8 @@ end
 index = randi(BVAR.ndraws,NumbDrawsFromPost,1);
 NowCast = nan(T, ny, nD);
 Companion_matrix = diag(ones(ny*(lags-1),1),-ny);
+frcsts_no_shock = nan(fhor, ny, NumbDrawsFromPost); 
+frcsts_with_shocks = frcsts_no_shock;
 
 if fast_kf
     % find the last full raw wihtout nans
@@ -33,6 +38,8 @@ if fast_kf
         [ ~, KFzero] = kfilternan(Phi, Sigma, y, KFoptions);
         pZero(:,:,i) = KFzero.CovSt(:,:,end);
         aZero(:,i)   = KFzero.smoothSt(end,:)';
+        forecast_data.initval = KFzero.smoothSt_plus_ss(end-lags+1:end,KFzero.index_var);%yfill(end-lags+1:end, :, d);
+        [frcsts_no_shock(:,:,i),frcsts_with_shocks(:,:,i)] = forecasts(forecast_data,Phi,Sigma,fhor,lags);
      end
 
 end
@@ -58,7 +65,6 @@ for j =  1: nD
             [~, KFout] = kfilternan(Phi, Sigma, data(To+1:end,:), KFoptions);
             NowCast(1:To,i,j) = KFzero.smoothSt_plus_ss(1:To,KFout.index_var(KFoptions.mf_varindex));
             NowCast(1+To:end,i,j) = KFout.smoothSt_plus_ss(:,KFout.index_var(KFoptions.mf_varindex));
-
         else
             [~, KFout] = kfilternan(Phi, Sigma, data, KFoptions);
             NowCast(:,i,j) = KFout.smoothSt_plus_ss(:,KFout.index_var(KFoptions.mf_varindex));
@@ -70,3 +76,5 @@ close(wb);
 
 out.NowCast = NowCast;
 out.options = options; 
+out.ForeCast.no_shock = [out.NowCast; squeeze(frcsts_no_shock(:,KFoptions.mf_varindex,:))] ;
+out.ForeCast.with_shocks = [out.NowCast; squeeze(frcsts_with_shocks(:,KFoptions.mf_varindex,:))] ;
